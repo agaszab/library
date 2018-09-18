6e938792dd2bfa478df17a2908bc08906392b101
agaszab <gatito@poczta.pl>
2018-09-18 09:23:12 +0200

    dorabianie wypożyczeń

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,18 @@
-INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (1, 'Sienkiewicz', 'PWN', 'Latarnik', 'powiesc', 2015);
+INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (1, 'Henryk Sienkiewicz', 'PWN', 'Latarnik', 'powiesc', 2015);
 INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (2, 'Augustyn', 'WAM', 'Wyznania', 'religia', 2001);
-INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (3, 'Staff', 'PWN', 'Wiersze wybrane', 'poezja', 1997);
+INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (3, 'Leopold Staff', 'PWN', 'Wiersze wybrane', 'poezja', 1997);
 INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (4, 'Socci', 'PWN', 'Tajemnice Jana Pawla II', 'religia', 2005);
+INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (5, 'Leopold Staff', 'PWN', 'Wiersze wybrane', 'poezja', 2000);
+INSERT INTO book (idb, author, publisher, title, category, publication_year) VALUES (6, 'Twardowski', 'PWN', 'Wiersze', 'poezja', 2005);
 INSERT INTO person (idp, first_name, last_name) VALUES (1, 'Jan', 'Kowalski');
 INSERT INTO person (idp, first_name, last_name) VALUES (2, 'Marek', 'Nowak');
 INSERT INTO person (idp, first_name, last_name) VALUES (3, 'Zosia', 'Zaremba');
 INSERT INTO person (idp, first_name, last_name) VALUES (4, 'Ala', 'Krokus');
 INSERT INTO person (idp, first_name, last_name) VALUES (5, 'Piotrek', 'Kowalski');
-INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (1, 20180412, 1,1, false);
-INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (2, 20180105, 1,3, true);
-INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (3, 20180612, 2,1, false);
-
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (1, 20180412, 3,1, false);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (2, 20180412, 1,6, false);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (3, 20180412, 1,5, false);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (4, 20180105, 1,3, true);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (5, 20180612, 2,2, false);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (6, 20180905, 1,2, true);
+INSERT INTO borrowed (id, date_borrowing, idp, idb, returned) VALUES (7, 20180905, 1,1, true);
